96fcb389b933056dc4875b497448cee4617a1994
AnastasiaSinitsyna <116564288+AnastasiaSinitsyna@users.noreply.github.com>
2022-12-08 19:57:02 +0100

    Исправление 1 по 2 Заданию

--- UnitTestProject1/UnitTestProject1/UnitTest2.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest2.cs
@@ -10,7 +10,7 @@ using OpenQA.Selenium.Support.UI;
 namespace addressbook_web_tests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class FirstTest
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,7 +40,7 @@ namespace addressbook_web_tests
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void NewClassAddressbookTest()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Clear();
